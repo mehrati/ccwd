0a00cab70c26281b42bd160b95a37e7f151539c0
mehrati <geekoffmail@gmail.com>
2018-07-11 19:42:00 +0430

    start project

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use std::env;
+use std::fs;
+use std::process::{exit, Command, Stdio};
+
+struct Clipboard<'a> {
+    cmd: String,
+    args: &'a [&'a str],
+}
+
+impl<'a> Clipboard<'a> {
+    fn new() -> Self {
+        if let Ok(n) = is_program_in_path("xsel") {
+            Clipboard {
+                cmd: n.to_owned(),
+                args: &["-b", "-i"],
+            }
+        } else {
+            if let Ok(n) = is_program_in_path("xclip") {
+                Clipboard {
+                    cmd: n.to_owned(),
+                    args: &["-selection", "c"],
+                }
+            } else {
+                println!("xclip and xset not found !!!");
+                println!("please install xclip or xset !!!");
+                exit(1);
+            }
+        }
+    }
+    fn run(self, input: String) {
+        let echo = Command::new("echo")
+            .arg("-n")
+            .arg(input)
+            .stdout(Stdio::piped())
+            .spawn()
+            .unwrap()
+            .stdout
+            .unwrap();
+        let cmd = Command::new(self.cmd)
+            .args(self.args)
+            .stdin(echo)
+            .spawn()
+            .unwrap();
+        cmd.wait_with_output().unwrap();
+    }
+}
+
+fn main() {
+    let clip = Clipboard::new();
+    clip.run(get_cwd_name());
+}
+fn get_cwd_name() -> String {
+    let cwd = env::current_dir().unwrap(); // env::var_os("PWD").unwrap()
+    let arg: Vec<String> = env::args().collect();
+    if arg.len() > 1 {
+        return cwd.join(&arg[1]).to_str().unwrap().to_owned();
+    }
+    cwd.to_str().unwrap().to_owned()
+}
+
+fn is_program_in_path(program: &str) -> Result<String, String> {
+    if let Ok(path) = env::var("PATH") {
+        for p in path.split(":") {
+            let p_str = format!("{}/{}", p, program);
+            if fs::metadata(p_str).is_ok() {
+                return Ok(program.to_owned());
+            }
+        }
+    }
+    Err(format!("{} not found !", program))
+}
